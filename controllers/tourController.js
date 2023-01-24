83018a2b0ee95f5715e5fc9146f8de686108e9e7
sanchitpasricha <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>
2023-01-24 11:06:14 +0530

    implementing pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -66,6 +66,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select("-__v");
     }
 
+    //5) Pagination
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist");
+    }
+
+    query = query.skip(skip).limit(limit);
+
     const tours = await query;
 
     res.status(200).json({
