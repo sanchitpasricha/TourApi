1b19fbd11f1e937c01a3b5ee48c2e1c633a294d8
sanchitpasricha <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>
2023-01-26 02:53:09 +0530

    Adding aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -101,3 +101,40 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.tourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: "$difficulty" },
+          num: { $sum: 1 },
+          numRatings: { $sum: "ratingQuantity" },
+          avgRating: { $avg: "ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" },
+        },
+      },
+      {
+        $sort: {
+          avgPrice: 1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      status: "success",
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid data sent!",
+    });
+  }
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
+router.route("/tour-stats").get(tourController.tourStats);
+
 router
   .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours);
